43d4d4d5db71955ff9a74990c1e6085419e633df
Black heart <szymon.m992@gmail.com>
2024-06-21 19:43:05 +0200

    Update ShootingSystem.cs

--- Assets/Scripts/Systems/ShootingSystem.cs
+++ Assets/Scripts/Systems/ShootingSystem.cs
@@ -1,32 +1,41 @@
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using UnityEngine;
+using Unity.Rendering;
 
 namespace DefenseGame
 {
+    [UpdateInGroup(typeof(SimulationSystemGroup), OrderFirst = true)]
     public partial struct ShootingSystem : ISystem
     {
         public void OnUpdate(ref SystemState state)
         {
-            var time = SystemAPI.Time.ElapsedTime;
-
-            foreach (var (inputs, shooter) in SystemAPI.Query<RefRO<InputsData>, RefRW<ShooterData>>())
             {
-                if (inputs.ValueRO.isShooting && (time - shooter.ValueRW.lastShotTime) >= shooter.ValueRW.shotCooldown)
+                var time = SystemAPI.Time.ElapsedTime;
+                var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
+
+                foreach (var (inputs, shooter) in SystemAPI.Query<RefRO<InputsData>, RefRW<ShooterData>>())
                 {
-                    shooter.ValueRW.lastShotTime = (float)time;
-                    var shellEntity = state.EntityManager.Instantiate(shooter.ValueRO.shellPrefab);
-
-                    // Get the LocalToWorld transform to obtain the world space position
-                    var shootingSpotTransform = state.EntityManager.GetComponentData<LocalToWorld>(shooter.ValueRO.shootingSpotEntity);
-
-                    state.EntityManager.SetComponentData(shellEntity, new LocalTransform
+                    if (inputs.ValueRO.isShooting && (time - shooter.ValueRW.lastShotTime) >= shooter.ValueRW.shotCooldown)
                     {
-                        Position = shootingSpotTransform.Position,
-                        Rotation = quaternion.identity,
-                        Scale = 1f
-                    });
+                        shooter.ValueRW.lastShotTime = (float)time;
+                        var shellEntity = ecb.Instantiate(shooter.ValueRO.shellPrefab);
+
+                        // Get the LocalToWorld transform to obtain the world space position
+                        var shootingSpotTransform = state.EntityManager.GetComponentData<LocalToWorld>(shooter.ValueRW.shootingSpotEntity);
+
+                        ecb.SetComponent(shellEntity, new LocalTransform
+                        {
+                            Position = shootingSpotTransform.Position,
+                            Rotation = quaternion.identity,
+                            Scale = 1f
+                        });
+                    }
                 }
+
+                ecb.Playback(state.EntityManager);
+                ecb.Dispose();
             }
         }
     }
