adf338085bec4481f7e62c49235cf242ee5c0774
Black heart <szymon.m992@gmail.com>
2024-06-21 13:49:13 +0200

    Initial commit

---
+++ Assets/Scripts/AuthoringAndMono/Character.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace DefenseGame
+{
+    public class Character : MonoBehaviour
+    {
+        public bool isPlayer;
+        public float movementSpeed = 2f;
+    }
+}
---
+++ Assets/Scripts/AuthoringAndMono/CharacterData.cs
@@ -0,0 +1,21 @@
+using Unity.Entities;
+
+namespace DefenseGame
+{
+    public struct CharacterData : IComponentData
+    {
+        public float movementSpeed;
+    }
+
+    public class CharacterBaker : Baker<Character>
+    {
+        public override void Bake(Character authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new CharacterData
+            {
+                movementSpeed = authoring.movementSpeed
+            });
+        }
+    }
+}
